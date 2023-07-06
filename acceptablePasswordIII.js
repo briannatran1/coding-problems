b30c7579cef81e9f5186e0e5e084e6487ffba7be
Brianna Tran <112038594+maserabri@users.noreply.github.com>
2023-07-06 14:06:08 -0700

    Create acceptablePasswordIII.js

---
+++ acceptablePasswordIII.js
@@ -0,0 +1,41 @@
+/*In this mission you need to create a password verification function.
+
+The verification conditions are:
+
+the length should be bigger than 6;
+should contain at least one digit, but cannot consist of just digits.
+Input: A string (string).
+
+Output: A logic value (boolean).
+
+Examples:
+
+assert.strictEqual(isAcceptablePassword("short"), false);
+assert.strictEqual(isAcceptablePassword("muchlonger"), false);
+assert.strictEqual(isAcceptablePassword("ashort"), false);
+assert.strictEqual(isAcceptablePassword("muchlonger5"), true);*/
+
+//check if password has at least 6 chars
+//set up test for 1 digit 
+//set up test for 1 non number
+//iterate through password using for...of loop 
+    //if char is number,
+        //set hasDigit to true
+    //else
+        //set nonDigit to true
+//return true if all 3 are true
+
+function isAcceptablePassword(password){
+    let minLength = password.length > 6;
+    let hasDigit = false;
+    let nonDigit = false;
+    for(let char of password){
+        if(!isNaN(Number(char))){
+            hasDigit = true;
+        }
+        else{
+            nonDigit = true;
+        }
+    }
+    return minLength && hasDigit && nonDigit;
+}
