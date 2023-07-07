109071aeae45a4ee313a342c44fd82799e8d7db6
Brianna Tran <112038594+maserabri@users.noreply.github.com>
2023-07-07 13:56:34 -0700

    Update anagramDifference.js

--- anagramDifference.js
+++ anagramDifference.js
@@ -5,7 +5,16 @@ Second word : ha c k er r a nk (6 letters removed)
 Result : 10*/
 
 //create freq maps for each word using helper function
-//
+//iterate over count1
+  //check if letter exists in count2
+  //if it doesn't exist,  
+    //add letter to removalCount
+  //else, it does exist
+    //absolute diff of freq count between letter is added to removalCount
+    //remove letter from count2 to mark it as processed
+//iterate over count2
+  //add remaining letters to removalCount
+//return removalCount
 
 function anagramDifference(w1,w2){
   let count1 = getLetterCount(w1);
