a8d9db3ed6103c04bc8282095ea49fcabc2b1bb5
Brianna Tran <112038594+briannatran1@users.noreply.github.com>
2023-07-10 23:14:48 -0700

    Create isNil.js

---
+++ isNil.js
@@ -0,0 +1,5 @@
+/*Write a function, isNil, which accepts a value, val. It should return true/false if the input is either null or undefined.*/
+
+function isNil(val) {
+    return val === null || val === undefined;
+}
