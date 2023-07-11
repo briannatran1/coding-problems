1dd483d3a78302fcc61799197a1dd8ac380ed7c5
Brianna Tran <112038594+briannatran1@users.noreply.github.com>
2023-07-11 00:03:18 -0700

    Create flatten.js

---
+++ flatten.js
@@ -0,0 +1,8 @@
+/*Write a function, flatten, which takes an array, items. It should “flatten” the array a single level. In other words, if there is one
+level of nesting in items, it should remove this one level of nesting.*/
+
+//use flat method to turn arr into 1D
+
+function flatten(items) {
+	return items.flat();
+}
